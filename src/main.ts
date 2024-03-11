6db0190143b247b9571fd0abceefa42672b90cf2
tgrall <tugdual@gmail.com>
2024-03-11 13:14:49 +0100

    id

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import * as core from '@actions/core'
-import * as github from '@actions/github'
 
 // impelment the github action that print the time in the console
-async function run() {
+async function run(): Promise<void> {
   try {
     const time = new Date().toTimeString()
     core.setOutput('time', time)
     core.info(`The time is ${time}`)
   } catch (error) {
-    let errorMessage = (error as Error).message
+    const errorMessage = (error as Error).message
     core.setFailed(errorMessage)
   }
+  return
 }
+
+run()
